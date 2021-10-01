b4457f720f8fa351f421b333c0b9184df1d2696f
Bru_9940 <bo.ischenko@mail.ru>
2021-10-01 10:46:01 +0300

    cleaning

--- PccNetworkHandler/src/ru/ischenko/gui/FilialGuiTable.java
+++ PccNetworkHandler/src/ru/ischenko/gui/FilialGuiTable.java
@@ -16,35 +16,26 @@ public class FilialGuiTable extends JPanel {
 /////////////////////////////////////////////////////////////////////////////////////////	
 	public class NetTableModel extends AbstractTableModel{
 		private final Vector<Network>	networks;
-		private final String[]			columnNames = new String[] 	{"Filial","Location","Address","Devices found"			};
-		private final Class[]			columnClass = new Class	[] 	{String.class, String.class, String.class, Boolean.class};
-		public NetTableModel(Vector<Network> network) {
-			this.networks = network;
-		}
+		private final String [ ]		columnNames = new String[ ] {	"Filial","Location","Address","Devices found"			};
+		private final Class  [ ]		columnClass = new Class	[ ] {	String.class, String.class, String.class, Boolean.class	};
+		/////////////////////////////////////////////////////////////////////////////////
+		public	NetTableModel(Vector<Network> network) { this.networks = network; }
 		@Override
 		public Object getValueAt(int rowIndex, int columnIndex) {
 			Network row = networks.get(rowIndex);
-	        if(0 == columnIndex) {
-	            return row.getFilial();
-	        }
-	        else if(1 == columnIndex) {
-	            return row.getLocation();
-	        }
-	        else if(2 == columnIndex) {
-	            return row.getAddress();
-	        }
-	        else if(3 == columnIndex) {
-	            return row.isDevicefull();
-	        }
+	        if		( 0 == columnIndex ) return row.getFilial		( );
+	        else if	( 1 == columnIndex ) return row.getLocation		( );
+	        else if ( 2 == columnIndex ) return row.getAddress		( );
+	        else if ( 3 == columnIndex ) return row.isDevicefull	( );
 			return null;
 		}
-		@Override public Class<?>	getColumnClass	(int columnIndex)	{ return columnClass[columnIndex];	}
-		@Override public String 	getColumnName	(int column) 		{ return columnNames[column];		}
-		@Override public int 		getRowCount		() 					{ return networks.size();			}
-		@Override public int 		getColumnCount	()					{ return columnNames.length;		}
+		@Override public Class<?>	getColumnClass	( int columnIndex )	{ return columnClass[ columnIndex ]; }
+		@Override public String 	getColumnName	( int column ) 		{ return columnNames[ column ];		 }
+		@Override public int 		getRowCount		( ) 				{ return networks.size( );			 }
+		@Override public int 		getColumnCount	( )					{ return columnNames.length;		 }
 	}
 /////////////////////////////////////////////////////////////////////////////////////////		
-	public FilialGuiTable(Vector<Network> networks){
+	public FilialGuiTable( Vector<Network> networks ){
 		super( new BorderLayout( ) );
 		NetTableModel tm = new NetTableModel( networks );
 		table = new JTable( tm );
@@ -53,6 +44,6 @@ public class FilialGuiTable extends JPanel {
 		setVisible( true );
 	}
 /////////////////////////////////////////////////////////////////////////////////////////
-	public JTable 	getTable	() 				{ return table; }
-	public void 	setTable	(JTable table)	{ this.table = table; }
+	public JTable 	getTable	( ) 				{ return table; }
+	public void 	setTable	( JTable table )	{ this.table = table; }
 }
--- PccNetworkHandler/src/ru/ischenko/logic/ParseNets.java
+++ PccNetworkHandler/src/ru/ischenko/logic/ParseNets.java
@@ -11,55 +11,41 @@ public class ParseNets {
  * ParseNets parsing data from networks file and populates Vector<Vector<String>>
  */
 	private Vector<Network> networks = new Vector<Network>();
-
+	/////////////////////////////////////////////////////////////////////////////////////
 	public ParseNets() {
-		final String FILIAL_PATTERN		="^#([^#]++)$";
-		final String LOCATION_PATTERN	="^##([^#]++)$";
-		final String NET_PATTERN		="^(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}/\\d{1,2})";
-		
-		Pattern filialPattern 		= Pattern.compile(FILIAL_PATTERN);
-		Pattern locationPattern 	= Pattern.compile(LOCATION_PATTERN);
-		Pattern netPattern			= Pattern.compile(NET_PATTERN);
-		
-		String[] parsed = {"","",""};		
-		
-		NetLoader rd = new NetLoader();
+		final String	FILIAL_PATTERN		= "^#([^#]++)$";
+		final String	LOCATION_PATTERN	= "^##([^#]++)$";
+		final String	NET_PATTERN			= "^(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}/\\d{1,2})";
+		Pattern			filialPattern 		= Pattern.compile	( FILIAL_PATTERN );
+		Pattern			locationPattern		= Pattern.compile	( LOCATION_PATTERN );
+		Pattern			netPattern			= Pattern.compile	( NET_PATTERN );
+		String[ ]		parsed				= { "", "", "" };		
+		NetLoader		rd 					= new NetLoader		();
 		rd.Read()
 		.forEach(line->{
-			Matcher filialMatcher = filialPattern.matcher(line);
-			if (filialMatcher.find()) {
-				parsed[FILIAL] = filialMatcher.group(1);
+			Matcher filialMatcher = filialPattern.matcher( line );
+			if ( filialMatcher.find( ) ) {
+				parsed[ FILIAL ] = filialMatcher.group( 1 );
 			} else {
-				if(parsed[FILIAL].isEmpty()) parsed[FILIAL] = UNKNOWN;
-				Matcher locationMatcher = locationPattern.matcher(line);
-				if(locationMatcher.find()) {
-					parsed[LOCATION] = locationMatcher.group(1);
+				if( parsed[ FILIAL ].isEmpty( ) ) parsed[ FILIAL ] = UNKNOWN;
+				Matcher locationMatcher = locationPattern.matcher( line );
+				if( locationMatcher.find( ) ) {
+					parsed[ LOCATION ] = locationMatcher.group( 1 );
 				} else {
-					if(parsed[LOCATION].isEmpty()) parsed[LOCATION] = UNKNOWN;
-					Matcher netMatcher = netPattern.matcher(line);
-					if(netMatcher.find()) {
-						parsed[NETWORK] = netMatcher.group(1);
-						Network parsedNet = new Network(parsed[FILIAL], parsed[LOCATION], parsed[NETWORK]);
-						parsed[LOCATION] = "";
-						parsed[NETWORK] = "";						
-						addNetwork(parsedNet);
-//						String netAddr = parsedNet.get(NETWORK);
-//						String[] netParts = netAddr.split("/");
-//						String[] netBytes = netParts[0].split("\\.");
-//						for(int i = 0; i < netBytes.length; i++) System.out.printf("%4.4s%s",netBytes[i],i==3?"/":".");
-//						System.out.printf("%2.2s\n",netParts[1]);
+					if( parsed[ LOCATION ].isEmpty( ) ) parsed[ LOCATION ] = UNKNOWN;
+					Matcher netMatcher = netPattern.matcher( line );
+					if( netMatcher.find( ) ) {
+						parsed[ NETWORK ] = netMatcher.group( 1 );
+						Network parsedNet = new Network( parsed[ FILIAL ], parsed[ LOCATION ], parsed[ NETWORK ] );
+						parsed[ LOCATION ] = "";
+						parsed[ NETWORK ] = "";						
+						addNetwork( parsedNet );
 					}
 				}
 			}
 		});
 	}
-	
-	public Vector<Network> getNetworks() {
-		return networks;
-	}
-	
-	public void addNetwork(Network net) {
-		getNetworks().add(net);
-	}
-	
+	/////////////////////////////////////////////////////////////////////////////////////
+	public Vector<Network> 	getNetworks	( ) 			{ return networks; }
+	public void				addNetwork	(Network net)	{ getNetworks().add(net);	}	
 }
