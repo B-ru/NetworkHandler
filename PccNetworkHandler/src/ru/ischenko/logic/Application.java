04f364e030aa62c7324a7616d2ea0f52ea3a9aa1
Bru_9940 <bo.ischenko@mail.ru>
2021-10-01 11:55:24 +0300

    cleaning

--- PccNetworkHandler/src/ru/ischenko/logic/Application.java
+++ PccNetworkHandler/src/ru/ischenko/logic/Application.java
@@ -6,7 +6,7 @@ public class Application {
 /**
  * Application class contain main method
  */
-	public static void main(String[] args) {
-		new NHFrame("Network Handler", new ParseNets().getNetworks());
+	public static void main( String[ ] args ) {
+		new NHFrame( "Network Handler", new ParseNets( ).getNetworks( ) );
 	}
 }
