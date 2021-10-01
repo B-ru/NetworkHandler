864bbca9d5407f71f081a8713caed20dcb784213
Bru_9940 <bo.ischenko@mail.ru>
2021-10-01 10:17:53 +0300

    cleaning

--- PccNetworkHandler/src/ru/ischenko/gui/FilialGuiTable.java
+++ PccNetworkHandler/src/ru/ischenko/gui/FilialGuiTable.java
@@ -11,38 +11,16 @@ import javax.swing.table.DefaultTableModel;
 import ru.ischenko.logic.Network;
 
 public class FilialGuiTable extends JPanel {
-	private static final long serialVersionUID = -1672772373453643620L;
-	private JTable table;
+	private static final long	serialVersionUID = -1672772373453643620L;
+	private JTable				table;
 /////////////////////////////////////////////////////////////////////////////////////////	
 	public class NetTableModel extends AbstractTableModel{
-		private final Vector<Network> networks;
-		private final String[] columnNames = new String[] {"Filial","Location","Address","Devices found"};
-		private final Class[] columnClass = new Class[] {String.class, String.class, String.class, Boolean.class};
-		
+		private final Vector<Network>	networks;
+		private final String[]			columnNames = new String[] 	{"Filial","Location","Address","Devices found"			};
+		private final Class[]			columnClass = new Class	[] 	{String.class, String.class, String.class, Boolean.class};
 		public NetTableModel(Vector<Network> network) {
 			this.networks = network;
 		}
-		
-		@Override
-		public Class<?> getColumnClass(int columnIndex) {
-			return columnClass[columnIndex];
-		}
-
-		@Override
-		public String getColumnName(int column) {
-			return columnNames[column];
-		}
-
-		@Override
-		public int getRowCount() {
-			return networks.size();
-		}
-
-		@Override
-		public int getColumnCount() {
-			return columnNames.length;
-		}
-
 		@Override
 		public Object getValueAt(int rowIndex, int columnIndex) {
 			Network row = networks.get(rowIndex);
@@ -60,21 +38,21 @@ public class FilialGuiTable extends JPanel {
 	        }
 			return null;
 		}
+		@Override public Class<?>	getColumnClass	(int columnIndex)	{ return columnClass[columnIndex];	}
+		@Override public String 	getColumnName	(int column) 		{ return columnNames[column];		}
+		@Override public int 		getRowCount		() 					{ return networks.size();			}
+		@Override public int 		getColumnCount	()					{ return columnNames.length;		}
 	}
 /////////////////////////////////////////////////////////////////////////////////////////		
 	public FilialGuiTable(Vector<Network> networks){
-		super(new BorderLayout());
-		NetTableModel tm = new NetTableModel(networks);
-		table = new JTable(tm);
-		table.setShowGrid(false);
-		add(table, BorderLayout.CENTER);
-		setVisible(true);
+		super( new BorderLayout( ) );
+		NetTableModel tm = new NetTableModel( networks );
+		table = new JTable( tm );
+		table.setShowGrid( false );
+		add( table, BorderLayout.CENTER );
+		setVisible( true );
 	}
 /////////////////////////////////////////////////////////////////////////////////////////
-	public JTable getTable() {
-		return table;
-	}
-	public void setTable(JTable table) {
-		this.table = table;
-	}
+	public JTable 	getTable	() 				{ return table; }
+	public void 	setTable	(JTable table)	{ this.table = table; }
 }
--- PccNetworkHandler/src/ru/ischenko/logic/Network.java
+++ PccNetworkHandler/src/ru/ischenko/logic/Network.java
@@ -9,41 +9,22 @@ package ru.ischenko.logic;
  */
 public class Network {
 
-	private String filial;
-	private String location;
-	private String address;
+	private String 	filial;
+	private String 	location;
+	private String 	address;
 	private boolean devicefull = false;
-	
-	
 	public Network(String filial, String location, String address) {
 		super();
 		setFilial(filial);
 		setLocation(location);
 		setAddress(address);
 	}
-	
-	public String getFilial() {
-		return filial;
-	}
-	public void setFilial(String filial) {
-		this.filial = filial;
-	}
-	public String getLocation() {
-		return location;
-	}
-	public void setLocation(String location) {
-		this.location = location;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public boolean isDevicefull() {
-		return devicefull;
-	}
-	public void setDevicefull(boolean devicefull) {
-		this.devicefull = devicefull;
-	}
+	public String 	getFilial		( )						{ return filial; }
+	public void 	setFilial		( String filial )		{ this.filial = filial; }
+	public String 	getLocation		( )						{ return location; }
+	public void 	setLocation		( String location )		{ this.location = location; }
+	public String 	getAddress		( )						{ return address; }
+	public void 	setAddress		( String address )		{ this.address = address; }
+	public boolean 	isDevicefull	( )						{ return devicefull; }
+	public void 	setDevicefull	( boolean devicefull )	{ this.devicefull = devicefull; }
 }
